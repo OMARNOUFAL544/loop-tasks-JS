3234f38a428d83e4bd1c81ab22e6e9a46e7daa24
OMARNOUFAL544 <omarnofal544@gmail.com>
2025-01-01 20:03:15 +0300

    Initial commit

---
+++ js 3.loop/javascript.js
@@ -0,0 +1,127 @@
+
+
+//     task 1
+
+
+
+// let text = "";
+
+// for (let i = 0; i <= 10; i++) {
+//   text += i +"";
+// }
+// document.write(text)
+
+
+
+//     task 2
+
+// let i = 1;
+
+//  while (i <= 10) {
+//     document.write(i)
+//    i++;
+// }
+
+
+
+//     task 3
+
+// const arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i <arr.length; i++ ){
+//     document.write(arr[i])
+// }
+
+
+//     task 4
+
+// for(let i = 0; i <= 10; i += 2){
+//     document.write(i)
+// }
+
+
+
+//     task 5
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++){
+//     sum += i;
+// }  
+//   document.write(sum)
+
+
+//     task 6
+
+// const arr = [1,2,3,4,5];
+// let largest = arr[0];
+// for (let i = 1; i <arr.length; i++){
+//     if (arr[i] > largest)
+//         largest = arr[i];
+// }
+// document.write(largest)
+
+
+//     task 7
+
+// const arr = [5,4,3,2,1];
+//  let smallest = arr[0];
+//  for (let i = 1; i <arr.length; i++){
+//      if (arr[i] < smallest)
+//          smallest = arr[i];
+//  }
+//   document.write(smallest)
+
+
+//     task 8
+
+// const arr = [1, 2, 3, 4, 5];
+//  let sum = 0;
+//  for (let i = 0; i < arr.length; i++){
+//      sum += arr[i];
+//  }
+//  const average = sum / arr.length;
+//   document.write(average);
+
+//     task 9 
+
+
+// const num = 5;
+// let factorial = 1; 
+// for (let i = 1; i <= num; i++){
+//     factorial *= i;
+// }
+// document.write(factorial)
+
+
+//     task 10
+
+// const limit = 20;
+// for (let i = 2; i <= limit; i++){
+//     let okay = true;
+//     for (let j = 2; j < i; j++){
+//         if (i % j === 0){
+//             okay = false;
+//         break;
+//         }
+// }
+// if (okay){
+//     document.write(i);
+// }
+// }
+
+
+
+//      task 11
+
+
+
+
+
+//      task 12
+
+
+// const arr = [1, 2, 3, 4, 5];
+// const step = 2;
+// for (let i = 0; i <arr.length; i += step){
+//     document.write(arr[i]);
+// }
+
